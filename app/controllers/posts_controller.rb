b5d7275fd1028c15602b288915e4c02c22d14b2a
Leland Jordon <lelandjordon@mac.com>
2016-04-21 16:02:11 -0400

    created first entry via rails console. Started routes file.

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,55 @@
+class EntrysController < ApplicationController
+
+  def index
+    @entrys = Entry.all
+  end
+
+  def show
+    @entry = Entry.find(params[:id])
+  end
+
+  def new
+    @entry = Entry.new
+  end
+
+  def create
+    @entry = Entry.new(entry_params)
+    if @entry.save
+      redirect_to @entry, notice: "New Entry successfully created"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @entry = Entry.find(params[:id])
+  end
+
+  def update
+    @entry = Entry.find(params[:id])
+    if @entry.update(entry_params)
+      redirect_to @entry
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @entry = Entry.find(params[:id])
+    @entry.destroy
+    flash[:notice] = "#{@entry.title} successfully deleted."
+    redirect_to @entry
+  end
+
+  private
+  def entry_params
+    params.require(:entry).permit(:body, :author, :title)
+  end
+
+
+
+
+
+
+
+end
